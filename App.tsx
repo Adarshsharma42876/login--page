e45b11763473bc7e706bd66e14c5016492697073
Riddhesh Desai <ambekar@Mayurs-Mac-mini.local>
2025-01-06 19:09:51 +0530

    feat: project setup and folder structure

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import LoginPage from "./src/components/screen/LoginPage";
+import LoginPage from "./src/screens/Auth/LoginScreen";
 
 const App = () => {
     return (
